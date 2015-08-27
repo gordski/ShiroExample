1309912c32c4a1ed2694168a49316608708afe95
Gordon Innes <gordski2k@yahoo.com>
2015-08-27 20:39:15 +0100

    Added EHCache.

--- src/main/java/shiroTest/TestApp.java
+++ src/main/java/shiroTest/TestApp.java
@@ -1,10 +1,12 @@
 package shiroTest;
 
+import ch.qos.logback.classic.Level;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.permission.DomainPermission;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.mgt.*;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.apache.shiro.subject.Subject;
@@ -12,6 +14,8 @@ import org.apache.shiro.util.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -41,13 +45,15 @@ public class TestApp
 
   public static void main(String[] args) throws InterruptedException
   {
+    ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+    root.setLevel(Level.TRACE);
 
     //
     // Setup shiro to use the TestRealm and enable caching.
     //
 
     // Create the realm and enable in memory cache.
-    TestRealm realm = new TestRealm(new MemoryConstrainedCacheManager());
+    TestRealm realm = new TestRealm(new EhCacheManager());
     realm.setAuthenticationCachingEnabled(true);
 
     // Create a security manager with the realm and bind it to the current thread.
@@ -57,6 +63,11 @@ public class TestApp
     // Ceate 'user1' subject by logging in with user name and password.
     Subject subject1 = new Subject.Builder().buildSubject();
     subject1.login(new UsernamePasswordToken("user1", "password"));
+
+    log.info("++++++++++++++++++++++++++++++++++++++++++++++++");
+
+    Subject subject1Again = new Subject.Builder().buildSubject();
+    subject1Again.login(new UsernamePasswordToken("user1", "password"));
 
     // Create 'user2' by assgin principal information and marking as authenticated.
     Subject subject2 = new Subject.Builder().principals(new SimplePrincipalCollection("user2", "User Number 2"))
@@ -86,5 +97,36 @@ public class TestApp
 
     exec.shutdown();
     exec.awaitTermination(10, TimeUnit.SECONDS);
+
+    TestResource resource1 = new TestResource("encoder", "dave");
+    TestResource resource2 = new TestResource("encoder", "bob");
+
+    Map<String, String> cfg1 = new HashMap<>();
+    cfg1.put("key1", "value1");
+    cfg1.put("key2", "value2");
+    cfg1.put("key3", "value3");
+
+    Map<String, String> cfg2 = new HashMap<>();
+    cfg2.put("restrictedKey", "value1");
+    cfg2.put("specialKey", "value2");
+
+    Map<String, String> cfg3 = new HashMap<>();
+    cfg3.put("specialKey", "specialValue");
+
+    ThreadContext.bind(subject1);
+    resource1.configure(cfg1);
+    resource1.configure(cfg2);
+    resource1.configure(cfg3);
+    resource2.configure(cfg1);
+    resource2.configure(cfg2);
+    resource2.configure(cfg3);
+
+    ThreadContext.bind(subject2);
+    resource1.configure(cfg1);
+    resource1.configure(cfg2);
+    resource1.configure(cfg3);
+    resource2.configure(cfg1);
+    resource2.configure(cfg2);
+    resource2.configure(cfg3);
   }
 }
--- src/main/java/shiroTest/TestRealm.java
+++ src/main/java/shiroTest/TestRealm.java
@@ -3,13 +3,15 @@ package shiroTest;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.authz.permission.DomainPermission;
 import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.*;
 
 public class TestRealm extends AuthorizingRealm
 {
@@ -30,9 +32,81 @@ public class TestRealm extends AuthorizingRealm
     if(principals.getPrimaryPrincipal().equals("user1"))
     {
       authz.addObjectPermission(new DomainPermission("log"));
+      authz.addStringPermission("encoder:*:config:specialKey");
+    }
+
+    if(principals.getPrimaryPrincipal().equals("user2"))
+    {
+      authz.addStringPermission("encoder:dave:config:*");
     }
 
     return authz;
+  }
+
+  private class TestPincipals implements PrincipalCollection
+  {
+    private final String username;
+
+    public TestPincipals(String username)
+    {
+      this.username = username;
+    }
+
+    @Override
+    public Object getPrimaryPrincipal()
+    {
+      return username;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T oneByType(Class<T> type)
+    {
+      return (T) (type == String.class ? username : null);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> Collection<T> byType(Class<T> type)
+    {
+      return (Collection<T>) (type == String.class ? Collections.singleton(username) : null);
+    }
+
+    @Override
+    public List asList()
+    {
+      return Collections.singletonList(username);
+    }
+
+    @Override
+    public Set asSet()
+    {
+      return Collections.singleton(username);
+    }
+
+    @Override
+    public Collection fromRealm(String realmName)
+    {
+      return Collections.singleton(username);
+    }
+
+    @Override
+    public Set<String> getRealmNames()
+    {
+      return Collections.singleton("TEST");
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+      return false;
+    }
+
+    @Override
+    public Iterator iterator()
+    {
+      return Collections.singleton(username).iterator();
+    }
   }
 
   /**
@@ -42,6 +116,19 @@ public class TestRealm extends AuthorizingRealm
   protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException
   {
     // Could check password, etc here.
-    return new SimpleAuthenticationInfo(token.getPrincipal(), token.getCredentials(), "test");
+    return new AuthenticationInfo()
+    {
+      @Override
+      public PrincipalCollection getPrincipals()
+      {
+        return new TestPincipals((String)token.getPrincipal());
+      }
+
+      @Override
+      public Object getCredentials()
+      {
+        return token.getCredentials();
+      }
+    };
   }
 }
